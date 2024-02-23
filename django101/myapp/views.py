73393d7d14c76ac72e559969f27e3d9376a8095d
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-02-24 00:17:24 +0300

    Html files(template) added.

--- django101/myapp/views.py
+++ django101/myapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, "home.html")
+
 
