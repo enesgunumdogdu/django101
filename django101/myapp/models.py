584993ffae4bc4992bbb9a9add5b4ad887a96f38
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-02-27 22:05:50 +0300

    Migration typo

--- django101/myapp/models.py
+++ django101/myapp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class TodoItem(models.Model):
-    title = models.CharField(max_Length=200)
+    title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
